d80db715993d69329c5793a3e784f96780614114
Margherita Donnici <margheritadonnici@gmail.com>
2020-09-08 20:56:07 +0200

    Finished problem initialization

--- main.cpp
+++ main.cpp
@@ -10,38 +10,52 @@ using namespace std;
  * the standard input according to the problem statement.
  **/
 struct TrafficLight {
+
+    TrafficLight(int distance, int duration) : distance(distance), duration(duration) {}
+
     int distance;
     int duration;
     vector<int> greenSpeeds;
+
+    void print() {
+        cerr << "Traffic Light " << "distance:" << distance << " || duration:" << duration << endl;
+    }
 };
 
 class Problem {
 public:
     int maxSpeed;
+    int lightCount;
     vector<TrafficLight> trafficLights;
 
-    void parseInput() {
-        int speed;
-        cin >> speed; cin.ignore();
-        int lightCount;
-        cin >> lightCount; cin.ignore();
-        for (int i = 0; i < lightCount; i++) {
+    static Problem initializeProblem() {
+        Problem problem;
+        cin >> problem.maxSpeed; cin.ignore();
+        cin >> problem.lightCount; cin.ignore();
+        for (int i = 0; i < problem.lightCount; i++) {
             int distance;
             int duration;
             cin >> distance >> duration; cin.ignore();
-            cerr << distance << endl;
-            cerr << duration << endl;
+            problem.trafficLights.emplace_back(distance, duration);
         }
-        cerr << lightCount << endl;
-        cerr << speed << endl;
+
+        return problem;
+    }
+
+    void print(){
+        cerr << "Speed Limit:" << maxSpeed << endl;
+        cerr << "Light Count:" << lightCount << endl;
+        for (auto tf : trafficLights) {
+            tf.print();
+        }
 
     }
 };
 
 int main()
 {
-    Problem problem;
-    problem.parseInput();
+    Problem theProblem = Problem::initializeProblem();
+    theProblem.print();
 
 
     // Write an answer using cout. DON'T FORGET THE "<< endl"
